43156702967e8e8f1324d652b7b1c3420d3f387b
jai.coax <ibrahim@financialexpress.net>
2017-05-18 18:46:54 +0530

    Signed-off-by: jai.coax <ibrahim@financialexpress.net>

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    path = require('path');
     
 require('./api/model.js')();  
 
@@ -8,10 +9,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended : true
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+//app.use(express.static('./dist'));
+
+app.use(express.static(path.join(__dirname, 'dist'))); 
 
 app.use('/api/v1', require('./api/api.js')());
 
-app.use(express.static('./dist'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './dist/index.html'));
+}); 
 
 app.listen('3000', function(){
     console.log('Listening in the port 3000');
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
     redirectTo: '/users',
     pathMatch: 'full'
   },
+  {
+    path: '**',
+    redirectTo: '/users'
+  }
 ];
 
 @NgModule({
